7c9e228367598685bd2e1093ab346551aa714106
Rafal <drozdek@users.noreply.github.com>
2020-09-21 22:26:42 +0100

    Update addpointers.js
    add brake lines to improve readability

--- js/addpointers.js
+++ js/addpointers.js
@@ -1,22 +1,29 @@
   $.fn.addListviewPointers = function () {
+    
     var that = $(this);         
+    
     var goDown = function () {
         $(document).scrollTop(that.offset().top + that.outerHeight() - $(window).height());
       },
+        
       goUp = function () {
         $(document).scrollTop(that.offset().top - $('[data-role=header]').height());
       },
+        
         hideIndicators = function () {
           $('.arrowTop, .arrowDown').hide();
         },
+        
         showIndicators = function () {
           $('.arrowTop, .arrowDown').show();
         },
+        
           toggleIndicators = function (el1, el2) {
             $('.' + el1).hide();
             $('.' + el2).show();
           };
-                        
+             
+    
       that
         .append( $('<div>', {
               class: 'arrowTop'
@@ -36,27 +43,37 @@
 
     // DON'T apply solution for ul element with defined height
     if ( that < $(window).height()) {
-      $(document).off('scrollstart.SHOWBTTNS scroll.SHOWBTTNS').on('scrollstart.SHOWBTTNS scroll.SHOWBTTNS', function () {
-        hideIndicators();
+      $(document).off('scrollstart.SHOWBTTNS scroll.SHOWBTTNS').
+          on('scrollstart.SHOWBTTNS scroll.SHOWBTTNS', function () {
+            hideIndicators();
       });
       hideIndicators();
     }
+    
     // DO apply solution for UL element with the height NOT defined
     else {
       $('.arrowTop').hide();
-      var firstElementClone = that.find('li').data('list-divider') ? that.find('li').data('list-divider').first().clone() : '';
-      $(document).off('scrollstart.SHOWBTTNS scroll.SHOWBTTNS').on('scrollstart.SHOWBTTNS scroll.SHOWBTTNS', function () {
+      var firstElementClone = that.find('li').data('list-divider') ? 
+                that.find('li').data('list-divider').first().clone() : 
+                    '';
+      
+      $(document).off('scrollstart.SHOWBTTNS scroll.SHOWBTTNS').
+                      on('scrollstart.SHOWBTTNS scroll.SHOWBTTNS', function () {
+        
         /*
          *  component top
          */
         if ($(document).scrollTop() + $('[data-role="header"]').height() < that.offset().top) {
           toggleIndicators('arrowTop', 'arrowDown');
         }
+        
         /*
          *  component middle
          */
-        else if ($(document).scrollTop() + $('[data-role="header"]').height() > that.offset().top
-                                    && $(document).scrollTop() + $('[data-role="header"]').height() < that.offset().top + that.outerHeight() - $(window).height()) {          
+        else if ( $(document).scrollTop() + $('[data-role="header"]').height() > that.offset().top
+                      && $(document).scrollTop() + $('[data-role="header"]').height() < 
+                                                  that.offset().top + that.outerHeight() - $(window).height()) {       
+          
         /*
          *  add first-child LI clone to DOM 
          */
@@ -65,18 +82,24 @@
         }
           showIndicators();
         }
+        
         /*
          *  component bottom
          */
-        else if ($(document).scrollTop() + $('[data-role="header"]').height() >= that.offset().top + that.outerHeight() - $(window).height()) {
+        else if ($(document).scrollTop() + $('[data-role="header"]').height() >= 
+                                                    that.offset().top + that.outerHeight() - $(window).height()) {
           toggleIndicators('arrowDown', 'arrowTop');
         }
+        
       });
     }
+    
     that.on("click", ".arrowTop", function () {
       goUp();
     });
+    
     that.on("click", ".arrowDown", function () {
       goDown();
     });
+    
   };
